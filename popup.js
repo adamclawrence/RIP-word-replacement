d1245761c5b39dd9e5bb2007124199b0c8b408b3
adamclawrence <lawrence.adam99@gmail.com>
2016-01-24 09:21:16 -0500

    Attempt at add word functionality

--- popup.js
+++ popup.js
@@ -1,16 +1,31 @@
+
+var newTextElements = [];
 document.addEventListener('DOMContentLoaded', function() {
   var addButton = document.getElementById('add');
+
   addButton.addEventListener('click', function() {
-    window.open('https://www.facebook.com','_blank');
-
-
-  }, false);
-
-  var settingsButton = document.getElementById('settings');
-  settingsButton.addEventListener('click', function() {
-
-    window.open('options.html','_blank');
+    //window.open('https://www.facebook.com','_blank');
+    addWord();
+    getAllKeys(newTextElements);
 
   }, false);
+
+  // var settingsButton = document.getElementById('settings');
+  // settingsButton.addEventListener('click', function() {
+
+  //   window.open('options.html','_blank');
+
+  // }, false);
 
 }, false);
+
+function addWord() {
+var word = document.getElementById('wordToReplace').textContent;
+var replacement = document.getElementById('replacementWord').textContent;
+console.log(word);
+    chrome.storage.sync.set({word : replacement}, function() {
+          // Notify that we saved.
+          console.log('custom words saved');
+        });
+
+}
