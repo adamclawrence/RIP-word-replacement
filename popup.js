72c751157720e01f629381277fbcd300a57d3d78
adamclawrence <lawrence.adam99@gmail.com>
2016-01-23 22:58:59 -0500

    update pop up appearance
    pop up appearance looks much better.

--- popup.js
+++ popup.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
   var addButton = document.getElementById('add');
   addButton.addEventListener('click', function() {
+    window.open('https://www.facebook.com','_blank');
 
-    alert('hello');
 
   }, false);
 
   var settingsButton = document.getElementById('settings');
   settingsButton.addEventListener('click', function() {
+
+    window.open('options.html','_blank');
+
+  }, false);
+
+  var filterButton = document.getElementById('filter');
+  filterButton.addEventListener('click', function() {
+
+    // window.open('options.html','_blank');
 
   }, false);
 }, false);
