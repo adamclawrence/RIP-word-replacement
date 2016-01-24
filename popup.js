c4fb62f0e8249853ac68872629a28b85049dc3f7
adamclawrence <lawrence.adam99@gmail.com>
2016-01-23 21:22:04 -0500

    Preliminary work on pop up
    Most pop up stuff is complete. Waiting to hook it up to the settings
    page and the dictionary.

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-popup.js
+document.addEventListener('DOMContentLoaded', function() {
+  var addButton = document.getElementById('add');
+  addButton.addEventListener('click', function() {
+
+    alert('hello');
+
+  }, false);
+
+  var settingsButton = document.getElementById('settings');
+  settingsButton.addEventListener('click', function() {
+
+  }, false);
+}, false);
